f177ad4eb232262e7ed2ef60a4ff2180b8559156
Alex Lev <levzx18@gmail.com>
2022-12-02 14:30:32 +0100

    refactor: add error enum for cgroup

--- src/cgroup.rs
+++ src/cgroup.rs
@@ -1,6 +1,13 @@
-use anyhow::{anyhow, Result};
 use log::{error, info};
-use std::{ffi::CString, fs, path::PathBuf};
+use std::{fs, path::PathBuf};
+
+#[derive(Debug)]
+pub enum CgroupError {
+    InvalidName(&'static str),
+    IOError(std::io::Error),
+}
+
+type Result<T> = std::result::Result<T, CgroupError>;
 
 pub struct CgroupV2Builder {
     name: String,
@@ -52,57 +59,86 @@ impl CgroupV2Builder {
     pub fn create(&mut self) -> Result<CgroupV2> {
         let cgroup_path: PathBuf = PathBuf::from("/sys/fs/cgroup/");
 
-        fs::create_dir_all(&cgroup_path)?;
+        if let Err(e) = fs::create_dir_all(&cgroup_path) {
+            return Err(CgroupError::IOError(e));
+        }
 
         // Add cpu, cpuset, memory and pids controllers
-        fs::write(cgroup_path.join("cgroup.subtree_control"), "+cpu")?;
-        fs::write(cgroup_path.join("cgroup.subtree_control"), "+cpuset")?;
-        fs::write(cgroup_path.join("cgroup.subtree_control"), "+memory")?;
-        fs::write(cgroup_path.join("cgroup.subtree_control"), "+pids")?;
+        if let Err(e) = fs::write(cgroup_path.join("cgroup.subtree_control"), "+cpu") {
+            return Err(CgroupError::IOError(e));
+        }
+
+        if let Err(e) = fs::write(cgroup_path.join("cgroup.subtree_control"), "+cpuset") {
+            return Err(CgroupError::IOError(e));
+        }
+
+        if let Err(e) = fs::write(cgroup_path.join("cgroup.subtree_control"), "+memory") {
+            return Err(CgroupError::IOError(e));
+        }
+
+        if let Err(e) = fs::write(cgroup_path.join("cgroup.subtree_control"), "+pids") {
+            return Err(CgroupError::IOError(e));
+        }
 
         let new_group_path: PathBuf = cgroup_path.join(&self.name);
 
         if new_group_path.exists() {
             if !new_group_path.is_dir() {
                 error!(target:"cgroup", "{:?} exists but is not a directory", new_group_path);
-                return Err(anyhow!("Path exist but is not a directory"));
+                return Err(CgroupError::InvalidName(
+                    "Path exist but is not a directory",
+                ));
             }
         } else {
-            fs::create_dir(&new_group_path)?;
+            if let Err(e) = fs::create_dir(&new_group_path) {
+                return Err(CgroupError::IOError(e));
+            }
         }
 
         info!(target:"cgroup", "{:?} created", new_group_path);
 
         // Let's add all the pids in the cgroup
         for pid in &self.pids {
-            fs::write(
+            if let Err(e) = fs::write(
                 new_group_path.join("cgroup.procs"),
                 pid.to_string().as_str(),
-            )?;
+            ) {
+                error!(target:"cgroup_procs", "{}", e);
+                return Err(CgroupError::IOError(e));
+            }
         }
 
         // Set the cpu limits
         if let Some(n_cpus) = self.cpus {
-            fs::write(
+            if let Err(e) = fs::write(
                 new_group_path.join("cpuset.cpus"),
                 n_cpus.to_string().as_bytes(),
-            )?;
+            ) {
+                error!(target:"cgroup_cpus", "{}", e);
+                return Err(CgroupError::IOError(e));
+            }
         }
 
         // Set the memory limit
         if let Some(max_mem) = self.max_mem {
-            fs::write(
+            if let Err(e) = fs::write(
                 new_group_path.join("memory.max"),
                 max_mem.to_string().as_str(),
-            )?;
+            ) {
+                error!(target:"cgroup_mem", "{}", e);
+                return Err(CgroupError::IOError(e));
+            }
         }
 
         // Set the pids limit
         if let Some(max_pids) = self.max_pids {
-            fs::write(
+            if let Err(e) = fs::write(
                 new_group_path.join("pids.max"),
                 max_pids.to_string().as_bytes(),
-            )?;
+            ) {
+                error!(target:"cgroup_pids", "{}", e);
+                return Err(CgroupError::IOError(e));
+            }
         }
 
         Ok(CgroupV2 {
@@ -114,7 +150,10 @@ impl CgroupV2Builder {
 
 impl CgroupV2 {
     pub fn destroy(&self) -> Result<()> {
-        fs::remove_dir_all(&self.path)?;
+        if let Err(e) = fs::remove_dir_all(&self.path) {
+            error!(target:"cgroup_destroy", "{}", e);
+            return Err(CgroupError::IOError(e));
+        }
 
         Ok(())
     }
